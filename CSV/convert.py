f97f9f78fba451a10e8118fab1450f47398d6f93
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2024-03-12 19:14:24 +0300

    Added a way to update json

---
+++ CSV/convert.py
@@ -0,0 +1,33 @@
+import csv
+import json
+
+def csv_to_json(csv_file, json_file):
+    data = {}
+    with open(csv_file, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Пропускаем заголовок
+
+        # Проходимся по каждой строке, собирая о словах
+        for row in reader:
+            """
+            Файлы CSV устроены следующим образом
+            Слово,Транскрипция,Перевод
+            """
+            word = row[0]
+            translation = row[1]
+            transcription = row[2]
+
+            # записываем новое слово
+            data[word] = {'translation': translation,
+                          'transcription': transcription}
+
+    # заполненный словарь записываем в json
+    with open(json_file, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
+"""
+всего проходимся по четырем файлам, 
+так как у нас в боте изучаются четыре экзамена 
+"""
+for i in range(1, 4+1):
+    csv_to_json(f"CSV\HSK{i}.csv", f"HSK{i}.json")
